4686a85897df2f5000f95c2454f4546f4271744e
redhawkx <joshua120496@gmail.com>
2021-03-30 22:45:05 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Home from "./Home.svelte";
+	import Contact from "./Contact.svelte";
+	import Router from "svelte-spa-router";
+</script>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
+
+<Router routes = {{
+	'/': Home,
+	'/contact': Contact
+}} />
